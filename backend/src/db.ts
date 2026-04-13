7fa0a1c901858d7bc5fb3fed488c37882dfda641
Valtair Guarnieri <139462265+valtairguarnieri@users.noreply.github.com>
2026-04-13 12:42:06 -0300

    Update db.ts

--- backend/src/db.ts
+++ backend/src/db.ts
@@ -1,6 +1,6 @@
 import { Pool } from 'pg';
 
 export const pool = new Pool({
-  connectionString: process.env.DATABASE_URL || 'postgresql://postgres:postgres@localhost:5432/sistema',
+  connectionString: process.env.DATABASE_URL || 'postgresql://postgres:fWOtXTIEGNvJGoIYTqdGCrGQaseWIfRI@monorail.proxy.rlwy.net:16293/railway',
   ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false,
 });
